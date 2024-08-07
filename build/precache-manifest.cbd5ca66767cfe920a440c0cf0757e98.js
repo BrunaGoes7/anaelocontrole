a289a64c088189da87c09477562923c2fe76b0d1
BrunaGoes7 <fbrunadegoes@yahoo.com.br>
2024-08-06 21:19:03 -0300

    Add build folder

---
+++ build/precache-manifest.cbd5ca66767cfe920a440c0cf0757e98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68842897086bbfd0f91decf314dde2c8",
+    "url": "/anaelocontrole/index.html"
+  },
+  {
+    "revision": "4b5889d7e9856e4de9cf",
+    "url": "/anaelocontrole/static/css/main.d8af9f0d.chunk.css"
+  },
+  {
+    "revision": "bde07599340d884e026d",
+    "url": "/anaelocontrole/static/js/2.6a092606.chunk.js"
+  },
+  {
+    "revision": "4b5889d7e9856e4de9cf",
+    "url": "/anaelocontrole/static/js/main.0adfc49d.chunk.js"
+  },
+  {
+    "revision": "0941ec03025aa18a4538",
+    "url": "/anaelocontrole/static/js/runtime~main.3fa1bcd9.js"
+  },
+  {
+    "revision": "3e6085510cbb36591ab143f4edb913c7",
+    "url": "/anaelocontrole/static/media/logo.3e608551.png"
+  }
+]);
